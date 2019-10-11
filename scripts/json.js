0e0e585cb44f6b3f3f63064ed24d408d9415fd73
tyson <taylort82@gmail.com>
2019-10-10 22:08:57 -0600

    Finished JSON

--- scripts/json.js
+++ scripts/json.js
@@ -1,8 +1,11 @@
+var myObj = {"make":"Ford","model":"Super Duty","year":2019};
+
 function jsonParseDemo () {
-    var myObj = JSON.parse(document.getElementById("string").innerHTML);
-    document.getElementById("demo").innerHTML = myObj.make + ", " + myObj.model + ", " + myObj.year;
+    var obj = JSON.parse(document.getElementById("string").innerHTML);
+    document.getElementById("demo").innerHTML = obj.make + ", " + obj.model + ", " + obj.year;
 }
 
 function jsonStringifyDemo () {
-    
+    var myText = JSON.stringify(myObj);
+    document.getElementById("demo1").innerHTML = myText;
 }
