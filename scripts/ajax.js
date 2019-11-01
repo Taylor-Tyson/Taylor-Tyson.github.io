9b528d49b8bfae95116ac6670fbe4dffc5e8b73f
tyson <taylort82@gmail.com>
2019-10-31 21:52:57 -0600

    Addec Chuck Norris api

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,11 +1,12 @@
 function docLoad() {
-    let url = "http://numbersapi.com/random/year?json";
+    // let url = "http://numbersapi.com/random/year?json";
+    let url = "https://api.chucknorris.io/jokes/random";
     let request = new XMLHttpRequest();
     
     request.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         let response = JSON.parse(this.responseText);
-        document.getElementById("demo").innerHTML = response.text;
+        document.getElementById("demo").innerHTML = response.value;
       }
     };
     request.open("GET", url, true);
