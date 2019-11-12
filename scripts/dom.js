6c0d1647c0e3b8f1e29897b02d5ebf1365ebdc16
tyson <taylort82@gmail.com>
2019-11-11 19:58:53 -0700

    Added DOM.

---
+++ scripts/dom.js
@@ -0,0 +1,14 @@
+function addToList() {   
+    let userInput = document.getElementById("myText").value;
+    let li = document.createElement("li");
+    let item = document.createTextNode(userInput);
+    li.appendChild(item);
+    document.getElementById("list").appendChild(li);
+    document.getElementById('myText').value = "";
+}
+
+function remove() {
+    let myList =  document.getElementById("list");
+    myList.removeChild(myList.childNodes[myList.childNodes.length-1]);
+    document.getElementById('myText').value = "";
+}
