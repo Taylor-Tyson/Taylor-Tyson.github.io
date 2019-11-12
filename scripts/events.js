60b8c71548b9a35332d9713679dc055822a8f01a
tyson <taylort82@gmail.com>
2019-11-11 22:36:38 -0700

    Adding events

---
+++ scripts/events.js
@@ -0,0 +1,31 @@
+function selection() {
+    let opt = document.getElementById("mySelection");
+    let choice = opt.options[opt.selectedIndex].value;
+
+    switch(choice){
+        case "Select Me":
+            document.getElementById("demo").innerHTML = "Why did you select me?";
+            break;
+        case "No Select Me":
+            document.getElementById("demo").innerHTML = "Thanks for selecting me. I didn't think you would do it.";
+            break;
+        case "You Better Select Me":
+            document.getElementById("demo").innerHTML = "You made the right choice by selecting me.";
+            break;
+        case "":
+            document.getElementById("demo").innerHTML = "";
+            break;
+    };
+}
+
+function clickMe() {
+    alert("You pressed the Click Me button. I hope you are happy now.");
+}
+
+function answer() {
+    document.getElementById("demo3").innerHTML = "Yesterday, Today, and Tomorrow.";
+}
+
+function mouseOut() {
+    document.getElementById("demo3").innerHTML = "";
+}
