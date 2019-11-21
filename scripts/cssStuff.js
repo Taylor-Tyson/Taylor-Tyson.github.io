83693d2164d01d408946497db86fe083eaaeb298
tyson <taylort82@gmail.com>
2019-11-20 19:43:13 -0700

    Finished Transformations

---
+++ scripts/cssStuff.js
@@ -0,0 +1,21 @@
+
+
+function clearMsg() {
+    document.getElementById("demo").innerHTML = "";
+}
+
+function rotateMsg() {
+    document.getElementById("demo").innerHTML = "This is an example of rotate.";
+}
+
+function skewMsg() {
+    document.getElementById("demo").innerHTML = "This is an example of skew.";
+}
+
+function scaleMsg() {
+    document.getElementById("demo").innerHTML = "This is an example of scaling.";
+}
+
+function spinMsg() {
+    document.getElementById("demo").innerHTML = "This is an example of smiley getting dizzy!.";
+}
