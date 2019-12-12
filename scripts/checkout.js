074215e0425d4a5ea363491d3de88cdf11cbf354
tyson <taylort82@gmail.com>
2019-12-12 04:10:14 -0700

    More project stuff

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 let myCart = JSON.parse(sessionStorage.getItem("cart"));
-var table = document.getElementById("cartTable");
+let table = document.getElementById("cartTable");
+let taxRate;
 
 //This sets the page on page load. 
 function setPage() {
@@ -30,30 +31,16 @@ function welcomeMessage(m) {
     + " parts will be on the way."
 }
 
+//This diplays the contents of the cart. 
 function showCart() {
-    // let myCart = JSON.parse(sessionStorage.getItem("cart"));
-    // var table = document.getElementById("cartTable");
-    // var row = table.insertRow(1);
-    // var cell1 = row.insertCell(0);
-    // var cell2 = row.insertCell(1);
-    // var cell3 = row.insertCell(2);
-    // let row = '';
-    // let cell1 = '';
-    // let cell2 = '';
-    console.log(myCart.length);
-    
-
+    // console.log(myCart.length);
     for (var i = 0; i < myCart.length; i++) {
-        // table.insertRow(i);
-        // cell1 = row.insertCell(i);
-        // cell1.innerHTML = myCart[i].itemName;
-        // cell2 = row.insertCell(i + 1);
-        // cell2.innerHTML = myCart[i].cost;
-        console.log(myCart[i].number);
+        // console.log(myCart[i].number);
         addRow(myCart[i].itemName, myCart[i].number, myCart[i].cost);
     }
 }
 
+//This adds rows to the table. 
 function addRow(item, num, cost) {
     let row = table.insertRow(1);
     let cell1 = row.insertCell(0);
@@ -64,3 +51,49 @@ function addRow(item, num, cost) {
     cell3.innerHTML = cost;
 }
 
+//This calculates the total cost. 
+function calculateTotal() {
+    let zip = document.getElementById("userZip").value;
+    let subTotal = 0;
+    let total = 0;
+    let salesTax = 0;
+
+    if (document.getElementById("userZip").value == "") {
+        alert("Please enzer zip code.")
+    }
+    else {
+        for (var i = 0; i < myCart.length; i++) {
+            subTotal += parseFloat(myCart[i].cost.replace(/[^0-9.-]+/g,""));
+        }
+
+        document.getElementById("subTotal").innerHTML = "Subtotal: " + subTotal.toFixed(2);
+        getTax();
+        document.getElementById("tax").innerHTML = "Tax rate: " + taxRate;
+        salesTax = (subTotal * taxRate);
+        total = subTotal + salesTax; 
+        sessionStorage.setItem("totalCost", parseFloat(total).toFixed(2));
+        document.getElementById("total").innerHTML = "Total Cost: $" + parseFloat(total).toFixed(2);
+    }
+}
+
+//This calls an API to get tax rate by zip code. 
+function getTax () {
+    let zip = document.getElementById("userZip").value;
+    let url = `https://trial.serviceobjects.com/ft/web.svc/JSON/GetBestMatch?&Zip=${zip}&TaxType=sales&LicenseKey=%20WS19-VOS2-URQ3%20`;
+    let request = new XMLHttpRequest();
+
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          let response = JSON.parse(this.responseText);
+          sessionStorage.setItem("taxRate", response.TaxInfoItems[0].TaxRate);
+          taxRate = response.TaxInfoItems[0].TaxRate;
+        }
+      };
+      request.open("GET", url, false);
+      request.send();
+    }
+
+//This will take you to the order page
+function submitOrder() {
+    location.replace("order.html")
+}
